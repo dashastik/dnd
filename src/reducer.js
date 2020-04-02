dc7fe0320c158abee5f6869c4489df3573a62fef
Darya <d.tsvetkova@storytelling-software.com>
2020-04-02 18:44:27 +0300

    character reducer added

---
+++ src/reducer.js
@@ -0,0 +1,55 @@
+const initState = {
+    characterName: '',
+    class: '',
+    level: 0,
+    background: '',
+    playerName: '',
+    race: '',
+    ideology: '',
+    masteryBonus: 0,
+
+    strength: 0,
+    strengthSave: false,
+    athletics: false,
+
+    dexterity: 0,
+    dexteritySave: false,
+    acrobatics: false,
+    sleightOfHand: false,
+    stealth: false,
+
+    construction: 0,
+    constructionSave: false,
+
+    intelligence: 0,
+    intelligenceSave: false,
+    investigation: false,
+    history: false,
+    arcana: false,
+    nature: false,
+    religion: false,
+
+    wisdom: 0,
+    wisdomSave: false,
+    perception: false,
+    survival: false,
+    medicine: false,
+    insight: false,
+    animalHandling: false,
+
+    charisma: 0,
+    charismaSave: false,
+    performance: false,
+    intimidation: false,
+    deception: false,
+    persuasion: false,
+}
+
+const character = (state = initState, action) => {
+    if (action.type === "CHANGE") {
+        return { ...state, [action.key]: action.value }
+    }
+    return state
+}
+
+export default character;
