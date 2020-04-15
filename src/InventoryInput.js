5807d0611768eb1a27dbbf038ef3add79f2fc2cb
Darya <d.tsvetkova@storytelling-software.com>
2020-04-15 16:15:29 +0300

    component InventoryInput added

---
+++ src/InventoryInput.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { InputGroup, Button, FormControl } from 'react-bootstrap';
+import { deleteInventoryItem, changeInventoryItem } from './action';
+import { connect } from 'react-redux';
+
+class InventoryInput extends Component {
+    handleClick = (e) => {
+        const action = deleteInventoryItem(this.props.id)
+        this.props.dispatch(action)
+    }
+    handleChange = (e) => {
+        const action = changeInventoryItem(this.props.id, e.target.value)
+        this.props.dispatch(action)
+    }
+    render() {
+        return (
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="Элемент инветаря"
+                    value={this.props.value}
+                    onChange={this.handleChange}
+                />
+                <InputGroup.Append>
+                    <Button variant="outline-secondary"
+                        onClick={this.handleClick}
+                    >х</Button>
+                </InputGroup.Append>
+            </InputGroup>
+
+        )
+    }
+}
+
+
+
+export default connect()(InventoryInput);
